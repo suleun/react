9b4fc8293042cd862713c09aa6f58be60b36b7bc
suleun <teaaa030@gmail.com>
2022-02-09 11:43:07 +0900

    props

--- cozy/src/App.js
+++ cozy/src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from './components/Counter'
 
 function App() {
+
+  const [buttonName, setButtonName] = useState('클릭');
+
+  const clickButton = () =>{
+    setButtonName('click');
+  }
 
   return (
     <div className="App">
     <h1>ggg</h1>
       
-    <Counter/><Counter/><Counter/>
+    <Counter click="click1"/>
+
+    <Counter click={buttonName}/>
+
+    <Counter/>
+
+    <button onClick={clickButton}>Click</button>
 
     </div>
   );
--- cozy/src/components/Counter.js
+++ cozy/src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Counter = () =>{
+const Counter = (props) =>{
     
   const [count, setCount] = useState(0);
 
@@ -8,9 +8,13 @@ const Counter = () =>{
     setCount(count + 1);
   }
 
+  const clickString = props.click || 'Click';
+
   return (
     <div className="App">
-    <button onClick={increment}>Click{count}</button>
+    <button onClick={increment}>
+    {clickString} {count}
+    </button>
     </div>
   );
 }
