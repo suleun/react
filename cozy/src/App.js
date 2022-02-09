f8cdf32bfc28fb843b3a31a7cc6b60b9ef55cba3
suleun <teaaa030@gmail.com>
2022-02-09 14:16:07 +0900

    form

--- cozy/src/App.js
+++ cozy/src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from './components/Movie'
 
 function App() {
 
-  const movies = [
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const[movies, setMovies] = useState([
     {title : 'sosoeueun', year:2021},
     {title : 'sosoeueun1', year:2022},
     {title : 'sosoeueun2', year:2023}
-  ]
+  ])
+
+  useEffect(()=>{
+    console.log('render')
+  })
 
   const renderMovies = movies.map(movie =>{
     return (
@@ -15,9 +21,42 @@ function App() {
     );
   })
 
+  const addMovie = (event) => {
+    event.preventDefault();
+    setMovies([
+      ...movies,
+      {
+        title:movieTitle,
+        year:movieYear
+      }
+    ])
+
+    setMovieTitle('');
+    setMovieYear('');
+  }
+
   return (
     <div className="App">
       <h1>Movie list</h1>
+
+      <form onSubmit={addMovie}> 
+        <input type="text"
+          value={movieTitle}
+          placeholder="영화 제목"
+          onChange={e => setMovieTitle(e.target.value)}
+        />
+        <br/>
+
+        <input type="text"
+          value={movieYear}
+          placeholder="개봉년도"
+          onChange={e => setMovieYear(e.target.value)}
+        />
+        <br/>
+
+        <button type="submit">영화 추가</button>
+
+      </form>
     
     {renderMovies}
 
